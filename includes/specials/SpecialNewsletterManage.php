e745460ccacc32a1936e2e4fc2afdf79053ccecb
Glaisher <glaisher.wiki@gmail.com>
2015-11-12 21:53:40 +0500

    Remove announce form from Special:ManageNewsletter
    Depends on I3801c5ad3a66214d45bbd
    
    Bug: T117230
    Change-Id: Icf8483e918c8289c985b160f04c6d9225f6c57dd

--- includes/specials/SpecialNewsletterManage.php
+++ includes/specials/SpecialNewsletterManage.php
@@ -20,19 +20,10 @@ class SpecialNewsletterManage extends SpecialPage {
 		$output->setSubtitle( NewsletterLinksGenerator::getSubtitleLinks( $this->getContext() ) );
 		$this->requireLogin();
 
-		# Create HTML forms
-		$announceIssueForm = new HTMLForm(
-			$this->getAnnounceFormFields(),
-			$this->getContext(),
-			'newsletter-announceissueform'
-		);
-		$announceIssueForm->setSubmitCallback( array( $this, 'onSubmitIssue' ) );
-
 		$pager = new NewsletterManageTablePager();
 		if ( $pager->getNumRows() > 0 ) {
 			$output->addParserOutput( $pager->getFullOutput() );
 			// Show HTML forms
-			$announceIssueForm->show();
 
 			if ( $this->getUser()->isAllowed( 'newsletter-addpublisher' ) ) {
 				// The user have required permissions
@@ -49,39 +40,6 @@ class SpecialNewsletterManage extends SpecialPage {
 			$output->showErrorPage( 'newslettermanage', 'newsletter-none-found' );
 		}
 
-	}
-
-	/**
-	 * Function to generate Announce Issue form
-	 *
-	 * @return array
-	 */
-	private function getAnnounceFormFields() {
-		$db = NewsletterDb::newFromGlobalState();
-		$userPublishedNewsletters = $db->getNewslettersUserIsPublisherOf( $this->getUser() );
-
-		$newsletterNameMap = array();
-		foreach ( $userPublishedNewsletters as $newsletter ) {
-			$newsletterNameMap[$newsletter->getName()] = $newsletter->getId();
-		}
-
-		return array(
-			'issue-newsletter' => array(
-				'type' => 'select',
-				'section' => 'announceissue-section',
-				'label-message' => 'newsletter-name',
-				'options' => array( $this->msg( 'newsletter-dropdown-default-message' )->text() => null ) + $newsletterNameMap,
-			),
-			'issue-page' => array(
-				'type' => 'text',
-				'section' => 'announceissue-section',
-				'label-message' => 'newsletter-issue-title',
-			),
-			'publisher' => array(
-				'type' => 'hidden',
-				'default' => $this->getUser()->getId(),
-			),
-		);
 	}
 
 	/**
@@ -110,67 +68,6 @@ class SpecialNewsletterManage extends SpecialPage {
 				'label-message' => 'newsletter-publisher-username',
 			),
 		);
-	}
-
-	/**
-	 * Perform insert query on issues table with data retrieved from HTML
-	 * form for announcing issues
-	 *
-	 * @param array $formData The data entered by user in the form
-	 *
-	 * @return array|bool true on success, array on error
-	 * @throws MWException
-	 */
-	public function onSubmitIssue( $formData ) {
-		if ( !isset( $formData['issue-page'] ) || !isset( $formData['issue-newsletter'] ) ) {
-			// This is not the form that was submitted
-			return false;
-		}
-
-		if ( !empty( $formData['issue-page'] ) && !empty( $formData['issue-newsletter'] ) ) {
-			$newsletterId = $formData['issue-newsletter'];
-			$issuePage = Title::newFromText( $formData['issue-page'] );
-			$pageId = $issuePage->getArticleId();
-			$pageNamepace = $issuePage->getNamespace();
-
-			// Array index is newsletter-id for selected newsletter in newsletterNames[] above
-			if ( ( $pageId !== 0 ) && isset( $newsletterId ) && isset( $formData['publisher'] ) ) {
-				$db = NewsletterDb::newFromGlobalState();
-				$issueCreated = $db->addNewsletterIssue(
-					$newsletterId,
-					$pageId,
-					$formData['publisher']
-				);
-
-				if ( !$issueCreated ) {
-					// TODO better output message here with i18n....
-					throw new MWException( 'Failed to create newsletter issue' );
-				}
-				$this->getOutput()->addWikiMsg( 'newsletter-issue-announce-confirmation' );
-
-				$newsletter = $db->getNewsletter( $newsletterId );
-
-				if ( class_exists( 'EchoEvent' ) ) {
-					EchoEvent::create(
-						array(
-							'type' => 'newsletter-announce',
-							'extra' => array(
-								'newsletter' => $newsletter->getName(),
-								'newsletterId' => $newsletter->getId(),
-								'issuePageTitle' => $formData['issue-page'],
-								'issuePageNamespace' => $pageNamepace,
-							),
-						)
-					);
-				}
-
-				return true;
-			} else {
-				return array( 'newsletter-issuepage-not-found-error' );
-			}
-		}
-
-		return array( 'newsletter-required-fields-error' );
 	}
 
 	/**
