b2fa68ce751364258de433490e9cb1a4ef80beb9
mhutti1 <mhutti1@gmail.com>
2015-12-07 22:46:13 +0000

    Adds Confirmation box to publisher removal
    Edits ext.newslettermanage.js to only remove
    publisher if the user confirms a popup message box.
    
    Bug: T108937
    Change-Id: I9c6949aadca3d1ab67054043914dc68763202b13

--- modules/ext.newslettermanage.js
+++ modules/ext.newslettermanage.js
@@ -6,19 +6,22 @@
 	'use strict';
 	var api = new mw.Api();
 	$( 'input[type=button]').click( function() {
-		var remNewsletterId = this.name;
-		var publisherId = this.id;
-		var $that = $( this );
+		var confirmBox = confirm("Are you certain you wish to remove this publisher?");
+		if (confirmBox === true) {
+			var remNewsletterId = this.name;
+			var publisherId = this.id;
+			var $that = $( this );
 
-		api.postWithToken( 'edit', {
-			action: 'newslettermanage',
-			publisher: publisherId,
-			id: remNewsletterId,
-			do: 'removepublisher'
+			api.postWithToken( 'edit', {
+				action: 'newslettermanage',
+				publisher: publisherId,
+				id: remNewsletterId,
+				do: 'removepublisher'
 
-		} ).done( function ( data ) {
-			mw.log( data );
-			$that.closest( 'tr' ).remove();
-		} );
+			} ).done( function ( data ) {
+				mw.log( data );
+				$that.closest( 'tr' ).remove();
+			} );
+		}
 	} );
 } )( jQuery, mediaWiki );
