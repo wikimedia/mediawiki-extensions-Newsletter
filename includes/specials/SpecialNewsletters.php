25784c0848b0d9bcbb2c8e087570368a28679612
Glaisher <glaisher.wiki@gmail.com>
2016-02-16 21:56:58 +0500

    Simplify Special:Newsletters code
    - Expand documentation
    - Don't instantiate HTMLForm for non-logged in users
    - Other minor cleanup
    
    Change-Id: I51c070b3af66a0dbe099fc401a254288ba9025a2

--- includes/specials/SpecialNewsletters.php
+++ includes/specials/SpecialNewsletters.php
@@ -1,15 +1,15 @@
 <?php
-
 /**
- * Special page for subscribing/un-subscribing a newsletter
+ * Implements Special:Newsletter which lists all the newsletters on the wiki.
+ * Logged-in users can also filter by subscribed/unsubscribed newsletters and
+ * also subscribe and unsubscribe from individual newsletters.
  *
  * @license GNU GPL v2+
  * @author Tina Johnson
  */
 class SpecialNewsletters extends SpecialPage {
-
 	/**
-	 * @var string
+	 * @var string $option Filter option for the table - doesn't affect anons
 	 */
 	private $option;
 
@@ -21,48 +21,48 @@ class SpecialNewsletters extends SpecialPage {
 		$this->setHeaders();
 
 		$out = $this->getOutput();
-		if ( $this->getUser()->isLoggedIn() ) {
-			// IPs cannot subscribe and these modules are only used for subscription functionality.
-			$out->addModuleStyles( 'ext.newsletter.newsletters.styles' );
-			$out->addModules( 'ext.newsletter.newsletters' );
-		}
-
 		$out->setSubtitle( NewsletterLinksGenerator::getSubtitleLinks( $this->getContext() ) );
 
 		$this->option = $this->getRequest()->getVal( 'filter', 'all' );
-		if ( !in_array( $this->option, array( 'all', 'subscribed', 'unsubscribed' ) ) ) {
-			// Set a default if invalid input was received from the user
+		$filtered = $this->option === 'subscribed' || $this->option === 'unsubscribed';
+		if ( !$filtered ) {
+			// Defaults to 'all' if unexpected input was received
 			$this->option = 'all';
 		}
 
-		$filterTableForm = HTMLForm::factory(
-			'ooui',
-			$this->getFormFields(),
-			$this->getContext()
-		);
-		$filterTableForm->setId( 'mw-newsletter-filter-form' );
-		$filterTableForm->setSubmitId( 'mw-newsletter-filter-submit' );
-		$filterTableForm->setMethod( 'get' );
-		$filterTableForm->setSubmitProgressive();
-		$filterTableForm->setSubmitTextMsg( 'newsletter-list-go-button' );
+		$formHtml = '';
+		if ( $this->getUser()->isLoggedIn() ) {
+			// Filter form and resource modules needed for logged-in users only
+			$out->addModuleStyles( 'ext.newsletter.newsletters.styles' );
+			$out->addModules( 'ext.newsletter.newsletters' );
+
+			$filterForm = HTMLForm::factory(
+				'ooui',
+				$this->getFormFields(),
+				$this->getContext()
+			);
+			$filterForm->setId( 'mw-newsletter-filter-form' );
+			$filterForm->setSubmitId( 'mw-newsletter-filter-submit' );
+			$filterForm->setMethod( 'get' );
+			// Note that submit button is hidden for users with JS enabled in
+			// as changing the dropdown menu's option updates the page for them
+			$filterForm->setSubmitProgressive();
+			$filterForm->setSubmitTextMsg( 'newsletter-list-go-button' );
+			$filterForm->prepareForm();
+			$formHtml = $filterForm->getHTML( false );
+		}
 
 		$pager = new NewsletterTablePager();
 		$pager->setUserOption( $this->option );
-
-		if ( $pager->getNumRows() === 0 && $this->option === 'all' ) {
+		if ( $pager->getNumRows() ) {
+			$out->addHTML( $formHtml );
+			$out->addParserOutput( $pager->getFullOutput() );
+		} elseif ( $filtered ) {
+			$out->addHTML( $formHtml );
+			$out->addWikiMsg( 'newsletter-list-search-none-found' );
+		} else {
 			// No newsletters exist on this wiki so just show an error page without the form
 			$out->showErrorPage( 'newsletters', 'newsletter-none-found' );
-			return;
-		}
-
-		if ( $this->getUser()->isLoggedIn() ) {
-			$filterTableForm->prepareForm()->displayForm( false );
-		}
-
-		if ( $pager->getNumRows() > 0 ) {
-			$out->addParserOutput( $pager->getFullOutput() );
-		} else {
-			$out->addWikiMsg( 'newsletter-list-search-none-found' );
 		}
 	}
 
