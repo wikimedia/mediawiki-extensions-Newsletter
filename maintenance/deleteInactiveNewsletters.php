b8d83ba4f871c1733e5e13017c6f2315559809b0
addshore <addshorewiki@gmail.com>
2016-04-02 15:17:17 +0300

    Add maint script to clear inactive newsletters
    Change-Id: I247ac21f3ecc9357fbfd8905c0cdab48563c0827

---
+++ maintenance/deleteInactiveNewsletters.php
@@ -0,0 +1,85 @@
+<?php
+
+$basePath = getenv( 'MW_INSTALL_PATH' ) !== false ? getenv( 'MW_INSTALL_PATH' ) : __DIR__ . '/../../..';
+
+require_once $basePath . '/maintenance/Maintenance.php';
+
+/**
+ * @author Addshore
+ */
+class DeleteInactiveNewsletters extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->addDescription(
+			"Deletes all inactive newsletters\nThese newsletters will no longer be restorable" );
+		$this->addOption( 'delete', 'Performs the deletion' );
+	}
+
+	public function execute() {
+		$dbw = $this->getDB( DB_MASTER );
+
+		if ( !$this->hasOption( 'delete' ) ) {
+			$count = $dbw->selectField(
+				'nl_newsletters',
+				'COUNT(*)',
+				array( 'nl_active' => 0 ),
+				__METHOD__
+			);
+			$this->output( "Found $count inactive newsletters to delete.\n" );
+			$this->output( "Please run the script again with the --delete option "
+				. "to really delete the revisions.\n" );
+			return;
+		}
+
+		$this->output( "Getting inactive newsletters...\n" );
+		$idsToDelete = $dbw->selectFieldValues(
+			'nl_newsletters',
+			'nl_id',
+			array( 'nl_active' => 0 ),
+			__METHOD__
+		);
+
+		if( !$idsToDelete ) {
+			$this->output( "No newsletters found to be deleted" );
+			return;
+		}
+
+		$dbw->delete(
+			'nl_newsletters',
+			'nl_id = ' .$dbw->makeList( $idsToDelete, LIST_OR ),
+			__METHOD__
+		);
+		$count = $dbw->affectedRows();
+		$this->output( "Deleted $count inactive newsletters.\n" );
+
+		$dbw->delete(
+			'nl_issues',
+			'nli_newsletter_id = ' .$dbw->makeList( $idsToDelete, LIST_OR ),
+			__METHOD__
+		);
+		$count = $dbw->affectedRows();
+		$this->output( "Deleted $count inactive newsletter issues.\n" );
+
+		$dbw->delete(
+			'nl_publishers',
+			'nlp_newsletter_id = ' .$dbw->makeList( $idsToDelete, LIST_OR ),
+			__METHOD__
+		);
+		$count = $dbw->affectedRows();
+		$this->output( "Deleted $count inactive newsletter publishers.\n" );
+
+		$dbw->delete(
+			'nl_subscriptions',
+			'nls_newsletter_id = ' .$dbw->makeList( $idsToDelete, LIST_OR ),
+			__METHOD__
+		);
+		$count = $dbw->affectedRows();
+		$this->output( "Deleted $count inactive newsletter subscriptions.\n" );
+
+		$this->output( "Done!\n" );
+	}
+}
+
+$maintClass = "DeleteInactiveNewsletters";
+require_once RUN_MAINTENANCE_IF_MAIN;
