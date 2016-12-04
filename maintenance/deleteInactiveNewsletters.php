eded832b419039b03c1d45b685342b7bac9540bf
Phantom42 <nikitav30@gmail.com>
2016-12-04 16:10:16 +0200

    Update maintenance script to use $this->requireExtension
    Update maintenance script deleteInactiveNewsletters.php to use $this->requireExtension()
    
    Bug: T152139
    Change-Id: I7b0650960c3de55359705b1699612d5f21b8521f

--- maintenance/deleteInactiveNewsletters.php
+++ maintenance/deleteInactiveNewsletters.php
@@ -14,6 +14,7 @@ class DeleteInactiveNewsletters extends Maintenance {
 		$this->addDescription(
 			"Deletes all inactive newsletters\nThese newsletters will no longer be restorable" );
 		$this->addOption( 'delete', 'Performs the deletion' );
+		$this->requireExtension( 'Newsletter' );
 	}
 
 	public function execute() {
