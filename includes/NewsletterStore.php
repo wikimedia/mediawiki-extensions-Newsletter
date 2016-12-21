a52924b2404a050e67848d53d8ccd4403d6c1487
Tony Thomas <01tonythomas@gmail.com>
2016-12-21 21:47:20 +0100

    Fix: Fatal on change of newsletter main page
    Bug: T153892
    Change-Id: I6834e35ba9ec0ebd27e23ff374747552b97335e5

--- includes/NewsletterStore.php
+++ includes/NewsletterStore.php
@@ -249,7 +249,7 @@ class NewsletterStore {
 	 * @throws MWException
 	 */
 	public function newsletterExistsForMainPage( $mainPageId ) {
-		return $this->db->newsletterExistsWithName( $mainPageId );
+		return $this->db->newsletterExistsForMainPage( $mainPageId );
 	}
 
 	/**
