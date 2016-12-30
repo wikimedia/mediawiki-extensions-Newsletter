41f38ff3daeef55f0e1730ada6f63f3f1af2cd00
rlot <rj23@protonmail.com>
2016-12-30 17:05:31 +0100

    Updated docs in EchoNewsletterUserLocator
    Bug: T154230
    Change-Id: I0addb017ff60f4d503c0b2b484a506ba41816ee5

--- includes/Echo/EchoNewsletterUserLocator.php
+++ includes/Echo/EchoNewsletterUserLocator.php
@@ -5,7 +5,7 @@ class EchoNewsletterUserLocator {
 	 * Locate all users subscribed to a newsletter.
 	 *
 	 * @param EchoEvent $event
-	 * @return User[]|array empty if the newsletter has been deleted/invalid
+	 * @return UserArrayFromResult|array empty if the newsletter has been deleted/invalid
 	 */
 	public static function locateNewsletterSubscribedUsers( EchoEvent $event ) {
 		$extra = $event->getExtra();
