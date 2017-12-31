310796c7cf820a7261206c90c5f8ac687249a7c2
Pppery <mapreader@olum.org>
2017-12-31 10:27:35 -0500

    Add unit tests for changing description and main page of newsletters via API
    Bug: T183817
    Bug: T183819
    Change-Id: I82dc3347b9ba1f7cc7787f85735b6237092f42cc

--- tests/content/NewsletterAPIEditTest.php
+++ tests/content/NewsletterAPIEditTest.php
@@ -51,4 +51,68 @@ class NewsletterAPIEditTest extends ApiTestCase {
 		$this->assertEquals( $newsletter->getPublishers(), $expectedUsers );
 		$this->assertEquals( $newsletter->getSubscribers(), $expectedUsers );
 	}
+
+	public function testUpdateDescription() {
+		# Set up by creating newsletter
+		$initialDescription = "A description that is at least 30 characters long";
+		$finalDescription = "A description that is still at least 30 characters long";
+		$mainPage = 'UTPage';
+		$mainPageId = Title::newFromText( $mainPage )->getArticleId();
+		$newsletter = new Newsletter( 0, 'Test', $initialDescription, $mainPageId );
+		NewsletterStore::getDefaultInstance()->addNewsletter( $newsletter );
+
+		# Modify the description
+		$newText = "{
+			\"description\": \"$finalDescription\",
+			\"mainpage\": \"$mainPage\",
+			\"publishers\": [
+				\"UTSysop\"
+			]
+		}";
+		$this->doApiRequestWithToken(
+			[
+				'action' => 'edit',
+				'title' => "Newsletter:Test",
+				'text' => $newText,
+			]
+		);
+
+		# Check the description
+		$newsletter = Newsletter::newFromName( "Test" );
+		$this->assertEquals( $newsletter->getDescription(), $finalDescription );
+	}
+
+	public function testUpdateMainPage() {
+		# Set up by creating newsletter
+		$description = "A description that is at least 30 characters long";
+		$oldMainPage = 'UTPage';
+		$oldMainPageId = Title::newFromText( $oldMainPage )->getArticleId();
+		$newsletter = new Newsletter( 0, 'Test', $description, $oldMainPageId );
+		$newMainPage = "SecondPage";
+		$newMainPageId = $this->insertPage( $newMainPage )["id"];
+		NewsletterStore::getDefaultInstance()->addNewsletter( $newsletter );
+
+		$newsletter = Newsletter::newFromName( "Test" );
+		$this->assertNotNull( $newsletter );
+
+		# Modify the main page
+		$newText = "{
+			\"description\": \"$description\",
+			\"mainpage\": \"$newMainPage\",
+			\"publishers\": [
+				\"UTSysop\"
+			]
+		}";
+		$this->doApiRequestWithToken(
+			[
+				'action' => 'edit',
+				'title' => "Newsletter:Test",
+				'text' => $newText,
+			]
+		);
+
+		# Check the main page
+		$newsletter = Newsletter::newFromName( "Test" );
+		$this->assertEquals( $newsletter->getPageId(), $newMainPageId );
+	}
 }
