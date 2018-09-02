f73fd8f1af257662634bada756b57f330a5dde35
Kunal Mehta <legoktm@member.fsf.org>
2018-09-01 19:58:20 -0700

    Remove unneccessary @suppress after phan-taint-check-plugin upgrade
    Bug: T201565
    Change-Id: I042172deeae674088e986b5de8461c68342c9e0a

--- includes/logging/NewsletterLogFormatter.php
+++ includes/logging/NewsletterLogFormatter.php
@@ -10,7 +10,6 @@ class NewsletterLogFormatter extends LogFormatter {
 
 	/**
 	 * Reformat the target as a user link if the target was a user
-	 * @suppress SecurityCheck-DoubleEscaped See T201565 for more information
 	 * @return array
 	 */
 	public function getMessageParameters() {
