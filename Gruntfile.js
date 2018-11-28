23efc9803711145cdd6253b73ba3105249185eac
James D. Forrester <jforrester@wikimedia.org>
2018-11-28 10:48:56 -0800

    eslint: Drop 'dot-notation' rule
    Bug: T210552
    Change-Id: Ib11866a4332df57f3d1b0ba90d82754337eb9f8c

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
 /* jshint node:true */
 module.exports = function ( grunt ) {
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
