dd4dd3694458a1752d723a853b15564221a6b0b8
Umherirrender <umherirrender_de.wp@web.de>
2019-05-18 17:51:11 +0200

    Add phan
    Change-Id: Ic726fe392bc84d8317a43eb48b68f717d83c668e

---
+++ .phan/config.php
@@ -0,0 +1,22 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+// T191666
+$cfg['suppress_issue_types'][] = 'PhanParamTooMany';
+
+$cfg['directory_list'] = array_merge(
+	$cfg['directory_list'],
+	[
+		'../../extensions/Echo',
+	]
+);
+
+$cfg['exclude_analysis_directory_list'] = array_merge(
+	$cfg['exclude_analysis_directory_list'],
+	[
+		'../../extensions/Echo',
+	]
+);
+
+return $cfg;
