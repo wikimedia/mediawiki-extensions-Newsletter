ef4d10c96db838df70c6236b32ac4f8d89a75d99
lens0021 <lorentz0021@gmail.com>
2021-05-30 02:36:29 +0900

    Korean translation for Newsletter special pages
    Change-Id: Ia7db276284b77a7d558ddac8cfbad46281901e9e

--- Newsletter.alias.php
+++ Newsletter.alias.php
@@ -12,12 +12,19 @@ $specialPageAliases = [];
 $specialPageAliases['en'] = [
 	'NewsletterCreate' => [ 'RegisterNewsletter', 'CreateNewsletter' ],
 	'Newsletters' => [ 'Newsletters' ],
-	'Newsletter' => [ 'Newsletter' ]
+	'Newsletter' => [ 'Newsletter' ],
 ];
 
 /** Hebrew (עברית) */
 $specialPageAliases['he'] = [
 	'NewsletterCreate' => [ 'יצירת_ידיעון' ],
 	'Newsletters' => [ 'ידיעונים' ],
-	'Newsletter' => [ 'ידיעון' ]
+	'Newsletter' => [ 'ידיעון' ],
+];
+
+/** Korean (한국어) */
+$specialPageAliases['ko'] = [
+	'NewsletterCreate' => [ '뉴스레터등록', '뉴스레터만들기' ],
+	'Newsletters' => [ '뉴스레터목록' ],
+	'Newsletter' => [ '뉴스레터' ],
 ];
