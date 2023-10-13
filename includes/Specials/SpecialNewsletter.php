580bce12484c4b7b153f3a2267ce71aca82d0e31
Umherirrender <umherirrender_de.wp@web.de>
2023-10-14 00:54:27 +0200

    Use the expression assignment operator to simplify code
    Suggested by phan, available since php7.4
    
    Change-Id: I5a6349f57a49c7ffc4a99cf2110b65a113213aa8

--- includes/Specials/SpecialNewsletter.php
+++ includes/Specials/SpecialNewsletter.php
@@ -64,7 +64,7 @@ class SpecialNewsletter extends SpecialPage {
 
 		// Separate out newsletter id and action from subpage
 		$params = explode( '/', $par );
-		$params[1] = $params[1] ?? null;
+		$params[1] ??= null;
 		list( $id, $action ) = $params;
 
 		$out = $this->getOutput();
