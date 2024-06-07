fa532e64f5e3b66ec5b2a62f9f517da7cd9b5e90
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2024-06-07 06:46:30 +0000

    build: Updating npm dependencies
    * eslint-config-wikimedia: 0.27.0 → 0.28.0
    * grunt-stylelint: 0.19.0 → 0.20.0
    * stylelint-config-wikimedia: 0.16.1 → 0.17.1
    
    Change-Id: I39de60957d68286f807cdc6e45e6e500609bab03

--- tests/selenium/specs/basic.js
+++ tests/selenium/specs/basic.js
@@ -3,8 +3,8 @@
 const assert = require( 'assert' ),
 	NewsletterPage = require( '../pageobjects/newsletter.page' );
 
-describe( 'Newsletter', function () {
-	it( 'page should exist on installation', async function () {
+describe( 'Newsletter', () => {
+	it( 'page should exist on installation', async () => {
 		await NewsletterPage.open();
 		assert.equal( await NewsletterPage.title.getText(), 'Newsletters' );
 	} );
